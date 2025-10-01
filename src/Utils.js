0cafbf91e7ac488e58048970bd5cee13440f09b2
yyrrcc <music_v@naver.com>
2025-10-01 14:30:40 +0900

    Editor 날짜 포맷팅

--- src/Utils.js
+++ src/Utils.js
@@ -23,3 +23,19 @@ export const getEmotionImgById = (emotionId) => {
       return null;
   }
 };
+
+// 날짜 포맷팅 해주는 함수 (yyyy-mm-dd)
+export const getFormattedDate = (targetDate) => {
+  let year = targetDate.getFullYear();
+  let month = targetDate.getMonth() + 1;
+  let date = targetDate.getDate();
+
+  if (month < 10) {
+    month = `0${month}`;
+  }
+  if (date < 10) {
+    date = `0${date}`;
+  }
+
+  return `${year}-${month}-${date}`;
+};
--- src/components/Editor.js
+++ src/components/Editor.js
@@ -1,12 +1,29 @@
 import "../components/css/Editor.css";
+import { useState } from "react";
+import { getFormattedDate } from "../Utils";
 
 // props : initData (수정 할 기존 입력된 내용), onSubmit (작성 완료 버튼)
 const Editor = ({ initData, onSubmit }) => {
+  // 1개의 일기를 1개의 객체로 설정하기
+  const [state, setState] = useState({
+    date: getFormattedDate(new Date()),
+    emotionId: 3,
+    content: "",
+  });
+
+  const handleChangeDate = (e) => {
+    // 객체 안에 있는 key를 가져와서 값 변경해주기!
+    setState({ ...state, date: e.target.value });
+  };
+
   return (
     <>
       <div className="Editor">
         <div className="editor_section">
           <h4>오늘의 날짜</h4>
+          <div className="input_wrapper">
+            <input type="date" value={state.date} onChange={handleChangeDate} />
+          </div>
         </div>
         <div className="editor_section">
           <h4>오늘의 감정</h4>
