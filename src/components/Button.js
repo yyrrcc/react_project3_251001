8dba19ac1fb1ebc974af0643ca5344ed59a0b5d0
yyrrcc <music_v@naver.com>
2025-10-01 12:13:02 +0900

    버튼 공통 컴포넌트 추가

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,9 +2,12 @@ import "./css/Button.css";
 
 // props : text (버튼 이름), type, 이벤트 핸들러
 const Button = ({ text, type, onclick }) => {
+  const btnType = ["positive", "negative"].includes(type) ? type : "default";
+
   return (
     <>
-      <button className="Button" onClick={onclick}>
+      {/* Button type에 따라 스타일을 변경하고 싶어서. Button Button_positive */}
+      <button className={["Button", `Button_${btnType}`].join(" ")} onClick={onclick}>
         {text}
       </button>
     </>
