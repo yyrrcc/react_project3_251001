0d953b9b19cf5da7e0848e6ab6b03e1ad99a72f5
yyrrcc <music_v@naver.com>
2025-10-02 11:47:06 +0900

    Home에 Header 부분 날짜 변경 버튼

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,13 +1,13 @@
 import "./css/Button.css";
 
 // props : text (버튼 이름), type, 이벤트 핸들러
-const Button = ({ text, type, onclick }) => {
+const Button = ({ text, type, onClick }) => {
   const btnType = ["positive", "negative"].includes(type) ? type : "default";
 
   return (
     <>
       {/* Button type에 따라 스타일을 변경하고 싶어서. Button Button_positive */}
-      <button className={["Button", `Button_${btnType}`].join(" ")} onClick={onclick}>
+      <button className={["Button", `Button_${btnType}`].join(" ")} onClick={onClick}>
         {text}
       </button>
     </>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,22 @@
+import { useContext, useState } from "react";
 import Button from "../components/Button";
 import Editor from "../components/Editor";
 import Header from "../components/Header";
+import { DiaryStateContext } from "../App";
 
+// context 설정이 됐기 때문에 App에서 보내준 props 사용 가능
 const Home = () => {
+  // 날짜 이동을 위한 변수
+  const [pivotDate, setPivotDate] = useState(new Date());
+  const headerTitle = `${pivotDate.getFullYear()}년 ${pivotDate.getMonth() + 1}월`;
+
+  const onIncreaseMonth = () => {
+    setPivotDate(new Date(pivotDate.getFullYear(), pivotDate.getMonth() + 1));
+  };
+  const onDecreaseMonth = () => {
+    setPivotDate(new Date(pivotDate.getFullYear(), pivotDate.getMonth() - 1));
+  };
+
   const onSubmit = () => {
     alert("일기 작성 완료");
   };
@@ -10,27 +24,14 @@ const Home = () => {
   return (
     <>
       <Header
-        title={"HOME"}
-        leftChild={
-          <Button
-            type={"positive"}
-            text={"긍정 버튼"}
-            onclick={() => {
-              alert("positive btn");
-            }}
-          />
-        }
-        rightChild={
-          <Button
-            type={"negative"}
-            text={"부정 버튼"}
-            onclick={() => {
-              alert("negative btn");
-            }}
-          />
-        }
+        title={headerTitle}
+        leftChild={<Button text={"<"} onClick={onDecreaseMonth} />}
+        rightChild={<Button text={">"} onClick={onIncreaseMonth} />}
       />
-      <Editor initData={{ date: new Date().getTime(), emotionId: 1, content: "이전에 작성한 일기" }} onSubmit={onSubmit} />
+      <Editor
+        initData={{ date: new Date().getTime(), emotionId: 1, content: "이전에 작성한 일기" }}
+        onSubmit={onSubmit}
+      />
     </>
   );
 };
