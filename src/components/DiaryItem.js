a0fc688f2a2a166a00bbff0a6c317da4ba4463e7
yyrrcc <music_v@naver.com>
2025-10-02 15:30:31 +0900

    Home (DiaryList, DiaryItem 완료)

--- src/components/DiaryItem.js
+++ src/components/DiaryItem.js
@@ -1,6 +1,7 @@
 import "./css/DiaryItem.css";
 import { getEmotionImgById } from "../Utils";
 import { useNavigate } from "react-router-dom";
+import Button from "./Button";
 
 const DiaryItem = ({ id, emotionId, content, date }) => {
   // 이미지 섹션을 클릭하면 해당 일기를 상세 조회할 수 있는 페이지로 이동
@@ -9,11 +10,23 @@ const DiaryItem = ({ id, emotionId, content, date }) => {
     navigate(`/diary/${id}`);
   };
 
+  // 수정하기 버튼 눌렀을 때
+  const goEdit = () => {
+    navigate(`/edit/${id}`);
+  };
+
   return (
     <>
       <div className="DiaryItem">
         <div onClick={goDetail} className={["img_section", `img_section_${emotionId}`].join(" ")}>
           <img src={getEmotionImgById(emotionId)} alt={`emotion${emotionId}`} />
+        </div>
+        <div className="info_section" onClick={goDetail}>
+          <div className="date_wrapper">{new Date(parseInt(date)).toLocaleDateString()}</div>
+          <div className="content_wrapper">{content.slice(0, 25)}</div>
+        </div>
+        <div className="button_section">
+          <Button onClick={goEdit} text={"수정하기"} />
         </div>
       </div>
     </>
